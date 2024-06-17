60bbb02cfa9906100fc8fff450ab9587d1b0f1ac
Jakub Koziel <jakubkoziel992@gmail.com>
2024-06-17 18:42:32 +0000

    changed configuration

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,11 +17,27 @@ resource "google_container_cluster" "gke_cluster" {
   name = "running-app-cluster"
   location = var.my_zone
 
+  remove_default_node_pool = true
   initial_node_count = 1
-  node_config {
-    machine_type = var.instance_type
-    disk_size_gb = 10
-  }
-
-  deletion_protection = false
 }
+
+resource "google_container_node_pool" "gke_nodes"{
+  name = "my-node-pool"
+  cluster = google_container_cluster.gke_cluster.name
+  node_count = var.my_nodes
+
+  node_config {
+    preemptible = true
+    machine_type = var.instance_type
+    disk_size_gb = var.disk_size
+  }
+}
+
+
+
+
+
+
+
+
+
