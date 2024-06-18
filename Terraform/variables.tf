f9d15e740307d04a8fdb4723408864fa1938380f
Jakub Koziel <jakubkoziel992@gmail.com>
2024-06-18 05:26:39 +0000

    added variables description

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,20 +1,24 @@
 variable "my_zone" {
+  description = "location used in projects"
   type = string
   default = "europe-central2"
 }
 
 variable "instance_type" {
+  description = "type of instance used in the cluster"
   type = string
   default = "e2-medium"
 }
 
 variable "disk_size" {
+  description = "disk size"
   type = string
   default = 10
 }
 
 variable "my_nodes" {
- type = string
- default = 0
+  description = "number of nodes used in cluster"
+  type = string
+  default = 1
 }
 
